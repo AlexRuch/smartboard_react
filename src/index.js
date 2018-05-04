9cf0c9dd02872a78f9941994abc6e8946aa91823
Alex <alexruchyov@gmail.com>
2018-05-04 19:53:28 +0300

    Add imports to index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ import thunk from 'redux-thunk';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import reducer from './reducers';
+import App from './containers/App';
+
+const stor = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Switch>
+            <Route path='/' component={App}/>  
+        </Switch>
+    </Provider>
+    document.getElementById('root')
+);
