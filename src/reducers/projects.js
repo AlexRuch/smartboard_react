ac32644ee3f9312e182ebd65d673da6e81d79516
Alex <alexruchyov@gmail.com>
2018-05-05 16:14:25 +0300

    Create projects reducer

---
+++ src/reducers/projects.js
@@ -0,0 +1,10 @@
+const initialState = {
+    entryList: [],
+    page: 0
+};
+
+export default function entries(stata = initialState, action) {
+    switch (action.type) {
+        case 'LOAD_ENTRIES':
+    }
+}
