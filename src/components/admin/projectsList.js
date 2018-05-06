8b55c4fc7ba61a2c5fa19d662c59f393660022bc
Alex <alexruchyov@gmail.com>
2018-05-06 12:18:46 +0300

    Add projects list

--- src/components/admin/projectsList.js
+++ src/components/admin/projectsList.js
@@ -8,16 +8,33 @@ export default class Projects extends React.Component {
         if (projectsList !== undefind && projectsList.length > 0) {
             projects = projectsList.map(project => {
                 <li class="proj-list__item">
-                    <div class="proj-list__left">
-                        <h2 class="proj-list__title">{ project.projectName }</h2>
-                        <p class="prj-list__description">{ project.porojectDescription }</p>
-                    </div>
-                    <div class="proj-list__right">
-                        <p class="proj-list__create"></p>
-                        <p class="proj-list__update"></p>
-                    </div>
+                    <Link to={ '/admin/project/' + project.projectId } className="proj-list__item">
+                        <div className="proj-list__left">
+                            <h2 className="proj-list__title">{ project.projectName }</h2>
+                            <p className="prj-list__description">{ project.porojectDescription }</p>
+                        </div>
+                        <div className="proj-list__right">
+                            <p className="proj-list__create">{ project.createDate }</p>
+                            <p className="proj-list__update">{ project.updateDate }</p>
+                        </div>
+                    </Link>
                 </li>
             });
         }
+
+        return (
+            <section className="projects">
+                <div className="project_navigation">
+                    <p>Сортировать по дате: </p>
+                    <button className="btn_sort">Создания</button>
+                    <button className="btn_sort">Редактирования</button>
+                    <p>Поиск по названию: </p>
+                    <input type="text"/>
+                </div>
+                <ul className="projects__list proj-list">
+                    { projects }
+                </ul>
+            </section>
+        );
     }
 }
